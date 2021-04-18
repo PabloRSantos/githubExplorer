c5ecb8dca73e3376b79d55c01a9d62680b4bfa12
PabloRSantos <pablorsantos15@gmail.com>
2021-04-18 13:36:32 -0300

    chore: setup

---
+++ src/routes/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Dashboard } from '../pages/Dashboard';
+import { Repository } from '../pages/Repository';
+
+const Routes: React.FC = () => (
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" component={Dashboard} exact />
+            <Route path="/repository" component={Repository} />
+        </Switch>
+    </BrowserRouter>
+);
+
+export { Routes };
